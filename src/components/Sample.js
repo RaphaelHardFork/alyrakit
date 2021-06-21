e7fd1fa5fc9736ff1056367f4c64b11b05f53c04
RaphaelHardFork <raphael.pellet@protonmail.com>
2021-06-21 15:33:01 +0200

    sample

--- src/components/Sample.js
+++ src/components/Sample.js
@@ -10,21 +10,19 @@ import {
 const Sample = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   return (
-    <div id="sample">
-      <Container>
-        <Text fontSize="xl" mb="6" textAlign="center">
-          Using AlyraKit to build your site means never worrying about designing
-          another page or cross browser compatibility. Our ever-growing library
-          of components and pre-designed layouts will make your life easier.
-        </Text>
-        <Center>
-          <Button onClick={onOpen} colorScheme="teal" size="lg">
-            Download a Sample
-          </Button>
-        </Center>
-        <Form isOpen={isOpen} onClose={onClose} />
-      </Container>
-    </div>
+    <Container py="20">
+      <Text fontSize="xl" mb="6" textAlign="center">
+        Using AlyraKit to build your site means never worrying about designing
+        another page or cross browser compatibility. Our ever-growing library of
+        components and pre-designed layouts will make your life easier.
+      </Text>
+      <Center>
+        <Button onClick={onOpen} colorScheme="teal" size="lg">
+          Download a Sample
+        </Button>
+      </Center>
+      <Form isOpen={isOpen} onClose={onClose} />
+    </Container>
   )
 }
 
